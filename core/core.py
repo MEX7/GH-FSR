4fcb72fb97d37e94c12cd790484a32be51e96d7a
MEX <mex7.0828@gmail.com>
2016-03-18 10:22:58 +0800

    提交两个基础脚本

---
+++ core/core.py
@@ -0,0 +1,20 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+
+@app.route('/script/oerp/v1/java/reload')
+def oerp_java_reload():
+    os.system('bash ./sh/java/oerp_v1_reload.sh')
+    return '/script/oerp/v1/java/reload'
+
+
+@app.route('/script/oerp/v1/web/reload')
+def oerp_web_world():
+    os.system('bash ./sh/web/oerp_v1_reload.sh')
+    return '/script/oerp/v1/web/reload'
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
---
+++ core/sh/java/oerp_v1_reload.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+# TODO 以后可以将地址以及要执行的脚本作为地址传入
+# 参数初始化
+project_name="outsourcing-erp-java"
+folder="/opt/script_log/"
+file=$(date +%Y-%m-%d)" ${project_name}.log"
+
+# 创建日志文件夹
+if [ ! -d "$folder" ]; then
+    mkdir "$folder"
+fi
+cd ${folder}
+
+# 创建当前日期的log文件
+if [ ! -f ${file} ]; then
+    touch ${file}
+fi
+
+# 写入启动数据
+echo $(date "+%Y-%m-%d %H:%M:%S")" $project_name oerp_v1_reload 脚本执行开始" >> "$file"
+
+cd /opt/${project_name}
+/bin/bash project/application/bin/app stop && git reset --hard && git pull
+/bin/bash project/application/bin/app start
+
+echo $(date "+%Y-%m-%d %H:%M:%S")" $project_name oerp_v1_reload 脚本执行成功" >> "$file"
